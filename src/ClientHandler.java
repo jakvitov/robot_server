00bf43c215592bd8a39476af197ba5aee4e6a6e4
Jakub Vítovec <vitovja1@fit.cvut.cz>
2022-04-16 10:37:28 +0200

    server_thread: Implemented function to extract the client name

--- src/ClientHandler.java
+++ src/ClientHandler.java
@@ -1,4 +1,5 @@
 import dataStruct.AutKeys;
+import dataStruct.Client;
 import dataStruct.Pair;
 
 import java.io.BufferedReader;
@@ -6,6 +7,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.StringTokenizer;
 
 public class ClientHandler implements Runnable{
 
@@ -14,6 +16,7 @@ public class ClientHandler implements Runnable{
     private Socket clientSocket;
     private AutKeys keyDatabase;
     private Pair autKey;
+    private Client client;
     private String suffix = "\\a\\b";
 
     public ClientHandler (Socket clientSocket){
@@ -26,6 +29,31 @@ public class ClientHandler implements Runnable{
             System.out.println("Error while opening the reader/writer!");
             System.exit(1);
         }
+    }
+
+    public boolean getClientName (){
+        String message = new String();
+
+        while ((message.contains(this.suffix) == false) && (message.length() < 21)){
+            try {
+                message += this.clientReader.readLine();
+            }
+            catch (IOException IOE){
+                System.out.println("Error while reading from the client socket!");
+                System.exit(1);
+            }
+        }
+
+        if (message.contains(this.suffix) == false || message.length() == this.suffix.length()){
+            System.out.println("Wrong input client name: " + message);
+            this.clientWriter.println("301 SYNTAX ERROR\\a\\b");
+            this.clientWriter.flush();
+            return false;
+        }
+
+        StringTokenizer tokenizer = new StringTokenizer(message, this.suffix);
+        this.client.username = tokenizer.nextToken();
+        return true;
     }
 
     @Override
---
+++ src/dataStruct/Client.java
@@ -0,0 +1,12 @@
+package dataStruct;
+
+/**
+ *A simple client class holding data of one client
+ */
+public class Client {
+
+
+    public String username;
+    public String keyNum;
+
+}
