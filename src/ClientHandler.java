bfc304b33c3adabe5f148ad155d0eca289e85789
Jakub Vítovec <vitovja1@fit.cvut.cz>
2022-04-16 10:08:00 +0200

    server_thread: Basic ClientHandler constructor

--- src/ClientHandler.java
+++ src/ClientHandler.java
@@ -1,4 +1,9 @@
+import dataStruct.AutKeys;
+import dataStruct.Pair;
+
 import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 
@@ -7,14 +12,24 @@ public class ClientHandler implements Runnable{
     private PrintWriter clientWriter;
     private BufferedReader clientReader;
     private Socket clientSocket;
+    private AutKeys keyDatabase;
+    private Pair autKey;
     private String suffix = "\\a\\b";
 
     public ClientHandler (Socket clientSocket){
-
+        try {
+            this.clientWriter = new PrintWriter(clientSocket.getOutputStream());
+            this.clientReader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            keyDatabase = new AutKeys();
+        }
+        catch (IOException IOE){
+            System.out.println("Error while opening the reader/writer!");
+            System.exit(1);
+        }
     }
 
     @Override
-    public void run(){
+    public void run (){
 
     }
 
